1884b75a32b27e24a1130b2be15efd9d5eacb8f1
Jebaseelan office <jebadjorah@gmail.com>
2024-03-22 13:08:04 +0400

    20240322 : from office pc

--- IMSAPI/Services/Administration/TokenService.cs
+++ IMSAPI/Services/Administration/TokenService.cs
@@ -2,6 +2,7 @@
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace IMSAPI.Services.Administration
@@ -44,6 +45,32 @@ namespace IMSAPI.Services.Administration
                 );
             return new JwtSecurityTokenHandler().WriteToken(jwtToken);
         }
-   
+        public static string GenerateRefreshToken()
+        {
+            var randomNumber = new byte[32];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
+        }
+        public static string GenerateAccessTokenFromRefreshToken(string refreshToken, string secret)
+        {
+            // Implement logic to generate a new access token from the refresh token
+            // Verify the refresh token and extract necessary information (e.g., user ID)
+            // Then generate a new access token
+
+            // For demonstration purposes, return a new token with an extended expiry
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secret);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Expires = DateTime.UtcNow.AddMinutes(15), // Extend expiration time
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
     }
 }
